4e6546fb87a5673220aff80305fc12556680c1ea
EC2 Default User <ec2-user@ip-172-31-29-144.us-east-2.compute.internal>
2021-03-11 08:24:20 +0000

    [HW4] code coverage

--- rottenpotatoes/spec/controllers/movies_controller_spec.rb
+++ rottenpotatoes/spec/controllers/movies_controller_spec.rb
@@ -2,24 +2,100 @@ require 'rails_helper'
 require 'monkey_patch'
 
 describe MoviesController, type: 'controller' do
+    fixtures :movies
+    
     describe 'movie has a director' do
         it 'should respond to the similar_movies action' do
-            movie1 = Movie.create!(id: 69, title: "Steve Jobs", director: "Danny Boyle")
-            get :similar_movies, { movie_id: movie1.id }
+            get :similar_movies, { movie_id: movies(:star_wars).id }
         end
         
         it 'assigns similar movies to the template' do
-          movie2 = Movie.create!(id: 89, title: "Star Wars", director: "George Lucas")
-          movie3 = Movie.create!(id: 90, title: "THX-1138", director: "George Lucas")
-          get :similar_movies, { movie_id: movie2[:id] }
-          expect(Movie.where(director: movie2.director).size).to eq(2)
+            get :similar_movies, { movie_id: movies(:star_wars).id }
+            expect(Movie.where(director: movies(:star_wars).director).size).to eq(2)
         end
     end
     
     describe 'movie does not have a director' do
         it 'should redirect to the index page on searching similar director movies' do
-            movie = Movie.create!(id: 67, title: "Steve Jobs", director: "")
-            get :similar_movies, { movie_id: movie.id }
+            get :similar_movies, { movie_id: movies(:steve_jobs).id }
+            expect(response).to redirect_to(movies_path)
+        end
+    end
+    
+    describe 'GET#index action' do
+        it 'should render the template view corresponding to index action' do
+            get :index
+            expect(response).to render_template('movies/index')
+        end
+        
+        it 'should fetch all the movies' do
+            expect(Movie).to receive(:all).and_return([movies(:star_wars), movies(:thx), movies(:steve_jobs), movies(:intersellar), movies(:blade_runner)])
+            get :index
+            expect(response.status).to eq(200)
+        end
+    end
+    
+    describe 'GET#new' do
+        let!(:movie) { Movie.new }
+    
+        it 'should render the template view corresponding to new action' do
+            get :new
+            expect(response).to render_template('movies/new')
+        end
+    end
+    
+    describe 'GET#show' do
+        before(:each) do
+            get :show, id: movies(:star_wars).id
+        end
+    
+        it 'should fetch the movie from the database' do
+            expect(assigns(:movie)).to eql(movies(:star_wars))
+        end
+    
+        it 'should render the template view corresponding to show action' do
+            expect(response).to render_template('movies/show')
+        end
+    end
+    
+    describe 'GET#edit' do
+        before do
+            get :edit, id: movies(:steve_jobs).id
+        end
+    
+        it 'should fetch the movie from the database' do
+            expect(assigns(:movie)).to eql(movies(:steve_jobs))
+        end
+    
+        it 'should render the template view corresponding to edit action' do
+            expect(response).to render_template('movies/edit')
+        end
+    end
+    
+    describe 'PUT#update' do
+        before(:each) do
+            put :update, id: movies(:intersellar).id, movie: { title: movies(:blade_runner).title, director: movies(:blade_runner).director }
+        end
+    
+        it 'should update the existing movie' do
+            movies(:intersellar).reload
+            expect(movies(:intersellar).title).to eql('Blade Runner')
+        end
+    
+        it 'should redirect to the show template of the movie' do
+            expect(response).to redirect_to(movie_path(movies(:intersellar)))
+        end
+    end
+    
+    describe 'DELETE#destroy' do
+        it 'should destroy a movie' do
+            expect {
+                delete :destroy, id: movies(:intersellar).id
+            }.to change(Movie, :count).by(-1)
+        end
+    
+        it 'redirects to movies#index after destroy' do
+            delete :destroy, id: movies(:intersellar).id
             expect(response).to redirect_to(movies_path)
         end
     end
--- rottenpotatoes/spec/models/movie_model_spec.rb
+++ rottenpotatoes/spec/models/movie_model_spec.rb
@@ -4,8 +4,8 @@ require 'monkey_patch'
 describe Movie do
   context 'multiple movies by the same director' do
     it 'matches movies with the same director' do
-      movie1 = Movie.create!(id: 79, title: "Star Wars", director: "George Lucas")
-      movie2 = Movie.create!(id: 80, title: "THX-1138", director: "George Lucas")
+      movie1 = Movie.create!(id: 179, title: "Star Wars", director: "George Lucas")
+      movie2 = Movie.create!(id: 180, title: "THX-1138", director: "George Lucas")
       results = Movie.similar_director_movies(movie1.title)
       expect(results).to eq([movie1, movie2])
     end        
@@ -13,8 +13,8 @@ describe Movie do
 
   context 'single movie by that director' do
     it 'does not matches any other movies with the same director' do
-      movie1 = Movie.create(id: 32, title: "Interstellar", director: "Christopher Nolan")
-      movie2 = Movie.create(id: 36, title: "Blade Runner", director: "Ridley Scott")
+      movie1 = Movie.create(id: 132, title: "Interstellar", director: "Christopher Nolan")
+      movie2 = Movie.create(id: 136, title: "Blade Runner", director: "Ridley Scott")
       results = Movie.similar_director_movies(movie1.title)
       expect(results).not_to eq([movie1, movie2])
     end
